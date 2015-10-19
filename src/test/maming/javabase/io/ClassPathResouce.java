baf4c5a553b5b6b6481022ecbad6c452d08bfb07
aixuebo <78608544@qq.com>
2015-10-19 19:28:26 +0800

    update

---
+++ src/test/maming/javabase/io/ClassPathResouce.java
@@ -0,0 +1,17 @@
+package maming.javabase.io;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ClassPathResouce {
+
+	//测试class是否在jar包内
+	@Test
+	public void classInJarTest(){
+		String classpath = "/org/apache/commons/cli/BasicParser.class";
+		Assert.assertEquals(ClassPathResouce.class.getResource(classpath).toString(),"jar:file:/D:/workspaceHive/hiveJdbc/lib/commons-cli-1.2.jar!/org/apache/commons/cli/BasicParser.class");
+		classpath = "/maming/ReadFile.class";
+		Assert.assertEquals(ClassPathResouce.class.getResource(classpath).toString(),"file:/D:/workspaceHive/hiveJdbc/bin/maming/ReadFile.class");
+		
+	}
+}
